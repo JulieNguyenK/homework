1252a3413e6ed37befbb5c08a29d9309baca081f
JulieNguyenK <nguyen.julie.kim@gmail.com>
2018-11-09 16:31:46 -0800

    reroll D10 on 1 or 2

--- diceJulie.py
+++ diceJulie.py
@@ -4,7 +4,7 @@ sum = 0
 
 for x in range(500000):
     roll = random.randint(1,10)
-    if roll == 1:
+    if roll == 1 or roll== 2:
         roll = random.randint(1, 10)
         sum += roll
     else:
