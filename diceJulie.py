f0ed4e3537c5fcb728ee756d3a354f7fe1b2d563
JulieNguyenK <nguyen.julie.kim@gmail.com>
2018-11-09 16:22:12 -0800

    D10 simulation

---
+++ diceJulie.py
@@ -0,0 +1,14 @@
+import random
+
+sum = 0
+
+for x in range(500000):
+    roll = random.randint(1,10)
+    if roll == 1:
+        roll = random.randint(1, 10)
+        sum += roll
+    else:
+        sum += roll
+
+    
+print (sum/ 500000)
